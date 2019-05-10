3c6c67d570839e5fd37c3b69f1601d44480a4335
Cynadyde <cynadydeindustries@gmail.com>
2019-05-10 01:11:25 -0400

    now 1.13 compatible

--- src/me/cynadyde/booklecterns/LecternPlugin.java
+++ src/me/cynadyde/booklecterns/LecternPlugin.java
@@ -2,6 +2,7 @@ package me.cynadyde.booklecterns;
 
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
 import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.utility.MinecraftReflection;
 import io.netty.buffer.ByteBuf;
@@ -34,6 +35,20 @@ import java.util.logging.Level;
  */
 @SuppressWarnings("WeakerAccess")
 public class LecternPlugin extends JavaPlugin implements Listener {
+
+    // Minecraft API & ProtocolLib fail us in 1.13.1...
+    private static String version;
+    private static boolean useReflection;
+    static {
+        try {
+            version = Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3];
+            int release = Integer.valueOf(version.split("_")[1]);
+            useReflection = release >= 13;
+        }
+        catch (Exception ex) {
+            useReflection = false;
+        }
+    }
 
     public static final String chatTag =
             ChatColor.DARK_GRAY + "[" + ChatColor.GRAY +
@@ -242,27 +257,80 @@ public class LecternPlugin extends JavaPlugin implements Listener {
      */
     public static void openBook(Player player, ItemStack book) {
 
-        int slot = player.getInventory().getHeldItemSlot();
-        ItemStack old = player.getInventory().getItem(slot);
+        // 0. "MC|BOpen is now "minecraft:book_open"
+        // 1. packet.getStrings().write(0, "minecraft:book_open") causes FieldAccessException "no field with type String exists in packet"
+        //    Does this mean CUSTOM_PAYLOAD doesn't have that field?
+        // 2. Is it possible to create a field for strings in the packet? Can't figure out.
+        // 3. ProtocolLib v4.5 has the packet type OPEN_BOOK, which fixes this issue, but cannot find the type for 1.13.
+        // 4. packet.getModifier().write(0, "minecraft:book_open") causes IllegalArgumentException
+        //    "cannot set MinecraftKey field with String"
+        // 5. Same happens when using ProtocolLib's MinecraftKey wrapper... it isn't an actual MinecraftKey obj
+//        int slot = player.getInventory().getHeldItemSlot();
+//        ItemStack old = player.getInventory().getItem(slot);
+//
+//        try {
+//            ByteBuf buffer = Unpooled.buffer(256);
+//            buffer.setByte(0, (byte) 0);
+//            buffer.writerIndex(1);
+//
+//            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+//            packet.getModifier().writeDefaults();
+//            packet.getModifier().write(0, new MinecraftKey("book_open"));
+//            packet.getModifier().write(1, MinecraftReflection.getPacketDataSerializer(buffer));
+//
+//            player.getInventory().setItem(slot, book);
+//            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
+//
+//        }
+//        catch (Exception ex) {
+//            Bukkit.getLogger().log(Level.SEVERE, "Unable to open book for " + player.getName(), ex);
+//        }
+//
+//        player.getInventory().setItem(slot, old);
+
+        // 6. Another option is to hardcode it for version 1.13.1 specifically.
+//        int handSlot = player.getInventory().getHeldItemSlot();
+//        ItemStack heldItem = player.getInventory().getItem(handSlot);
+//
+//        ByteBuf buffer = Unpooled.buffer(256).setByte(0, 0).writerIndex(1);
+//        MinecraftKey key = new MinecraftKey("minecraft:book_open");
+//        PacketDataSerializer serializer = new PacketDataSerializer(buffer);
+//        PacketPlayOutCustomPayload packet = new PacketPlayOutCustomPayload(key, serializer);
+//
+//        player.getInventory().setItem(handSlot, book);
+//        ((CraftPlayer)player).getHandle().playerConnection.sendPacket(packet);
+//        player.getInventory().setItem(handSlot, heldItem);
+
+
+        // 7. Use a bit of reflection to get a MinecraftKey...
+        ProtocolManager protocolManager = ProtocolLibrary.getProtocolManager();
+
+        int handSlot = player.getInventory().getHeldItemSlot();
+        ItemStack heldItem = player.getInventory().getItem(handSlot);
 
         try {
-            ByteBuf buffer = Unpooled.buffer(256);
-            buffer.setByte(0, (byte) 0);
-            buffer.writerIndex(1);
-
-            PacketContainer packet = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
+            ByteBuf buffer = Unpooled.buffer(256).setByte(0, (byte) 0).writerIndex(1);
+            Object serializer = MinecraftReflection.getPacketDataSerializer(buffer);
+            PacketContainer packet = protocolManager.createPacket(PacketType.Play.Server.CUSTOM_PAYLOAD);
             packet.getModifier().writeDefaults();
-            packet.getStrings().write(0, "MC|BOpen");
-            packet.getModifier().write(1, MinecraftReflection.getPacketDataSerializer(buffer));
 
-            player.getInventory().setItem(slot, book);
-            ProtocolLibrary.getProtocolManager().sendServerPacket(player, packet);
+            if (useReflection) {
+                Object key = Class.forName("net.minecraft.server." + version + ".MinecraftKey")
+                        .getConstructor(String.class).newInstance("minecraft:book_open");
+                packet.getModifier().write(0, key);
+            }
+            else {
+                packet.getStrings().write(0, "MC|BOpen");
+            }
+            packet.getModifier().write(1, serializer);
 
+            player.getInventory().setItem(handSlot, book);
+            protocolManager.sendServerPacket(player, packet);
         }
         catch (Exception ex) {
             Bukkit.getLogger().log(Level.SEVERE, "Unable to open book for " + player.getName(), ex);
         }
+        player.getInventory().setItem(handSlot, heldItem);
 
-        player.getInventory().setItem(slot, old);
     }
 }
